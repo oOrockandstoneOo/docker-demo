2520f87e0524657de26fb6ada6a492fbae713341
amarconiGIT <amarconi@ntonline.it>
2025-11-26 01:06:15 +0100

    Update server.py
    Added favicon exclusion

--- docker_demo_package/demo-compose/app/server.py
+++ docker_demo_package/demo-compose/app/server.py
@@ -1,9 +1,15 @@
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import redis, os
 
 r = redis.Redis(host=os.getenv("REDIS_HOST", "redis"), port=6379)
+
 class H(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == "/favicon.ico":
+            self.send_response(204)  # No Content
+            self.end_headers()
+            return
         count = r.incr("hits")
         msg = f"Hello from {os.getenv('SERVICE_NAME','web')} | hits={count}"
         self.send_response(200)
